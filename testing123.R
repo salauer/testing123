e63e6afbfb38c271555caa439ff21bbac1baced4
Stephen Lauer <Scuba@stephen-lauers-macbook-3.local>
2014-03-29 17:54:43 -0400

    1st commit

---
+++ testing123.R
@@ -0,0 +1,13 @@
+## Testing out some GitHub repository stuff with H & E
+
+library(XML)
+
+site <- "http://www.baseball-reference.com/postseason/2013_WS.shtml"
+
+tables <- readHTMLTable(site)
+
+Sox_Batting <- tables[[1]]
+
+greatest_DH_of_all_time <- Sox_Batting[which.max(Sox_Batting$OPS),1]
+
+greatest_DH_of_all_time
