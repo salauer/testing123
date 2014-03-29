65be8b4e981f7a66803360a2cd05213fb55e43df
salauer <stephenalauer@gmail.com>
2014-03-29 18:41:14 -0400

    2nd commit

--- testing123.R
+++ testing123.R
@@ -10,4 +10,4 @@ Sox_Batting <- tables[[1]]
 
 greatest_DH_of_all_time <- Sox_Batting[which.max(Sox_Batting$OPS),1]
 
-greatest_DH_of_all_time
+paste(greatest_DH_of_all_time, "is the greatest DH of all time")
